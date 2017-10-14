fcf7ddadf31b584e4d430db1e1b00a4dc59c3d2e
Avantika Chhabra <avantikachhabra93@gmail.com>
2017-10-15 04:23:19 +0530

    Add dialog for pause game and handle some back presses

--- app/src/main/java/com/example/avantika/clickit/GameActivity.java
+++ app/src/main/java/com/example/avantika/clickit/GameActivity.java
@@ -1,14 +1,17 @@
 package com.example.avantika.clickit;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -26,6 +29,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     String backColor = "#FFFFFF";
     ProgressBar pb;
     public static int totalTime = 15;
+    CountDownTimer timer;
+    long timeRemaining;
 
 
     //@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
@@ -67,8 +72,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                     LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f));
             parentLayout.addView(horLayout[i]);
 
-            for(int j = 0; j < orderGrid; j++)
-            {
+            for(int j = 0; j < orderGrid; j++) {
                 verLayout[i][j] = new LinearLayout(this);
                 verLayout[i][j].setId(i*orderGrid + j);
                 verLayout[i][j].setOrientation(LinearLayout.VERTICAL);
@@ -82,17 +86,21 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         cur = rand.nextInt(orderGrid*orderGrid);
         prev = cur;
         verLayout[cur/orderGrid][cur%orderGrid].setBackgroundColor(Color.parseColor(selectedColor));
-        new CountDownTimer(totalTime * 1000, 1000) {
+
+        timeRemaining = totalTime;
+        tvCount.setText("Seconds remaining: " + Long.toString(timeRemaining));
+
+        timer = new CountDownTimer(totalTime * 1000, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                long timeRemaining = millisUntilFinished / 1000;
+                timeRemaining = millisUntilFinished / 1000;
                 tvCount.setText("Seconds remaining: " + Long.toString(timeRemaining));
                 float percentageProgressBar = (((float) totalTime - (float) timeRemaining)/totalTime) * 100;
                 pb.setProgress((int) percentageProgressBar);
             }
 
             public void onFinish() {
-                long timeRemaining = 0;
+                timeRemaining = 0;
                 tvCount.setText("Seconds remaining: " + Long.toString(timeRemaining));
                 float percentageProgressBar = (((float) totalTime - (float) timeRemaining)/totalTime) * 100;
                 pb.setProgress((int) percentageProgressBar);
@@ -102,10 +110,12 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 finish();
             }
 
-        }.start();
+        };
+
+        timer.start();
     }
 
-   // @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    // @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onClick(View view) {
         int id = view.getId();
@@ -126,6 +136,84 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             prev=cur;
         }
 
+    }
+
+    void disableGrid(){
+        for (int j = 0; j< orderGrid; j++){
+            for (int k = 0; k < orderGrid; k++){
+//                verLayout[j][k].setClickable(false);
+                verLayout[j][k].setEnabled(false);
+            }
+        }
+    }
+
+    void enaableGrid(){
+        for (int j = 0; j< orderGrid; j++){
+            for (int k = 0; k < orderGrid; k++){
+//                verLayout[j][k].setClickable(false);
+                verLayout[j][k].setEnabled(true);
+            }
+        }
+    }
+
+    void resumeGame(){
+        enaableGrid();
+
+        timer = new CountDownTimer(timeRemaining * 1000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timeRemaining = millisUntilFinished / 1000;
+                tvCount.setText("Seconds remaining: " + Long.toString(timeRemaining));
+                float percentageProgressBar = (((float) totalTime - (float) timeRemaining)/totalTime) * 100;
+                pb.setProgress((int) percentageProgressBar);
+            }
+
+            public void onFinish() {
+                timeRemaining = 0;
+                tvCount.setText("Seconds remaining: " + Long.toString(timeRemaining));
+                float percentageProgressBar = (((float) totalTime - (float) timeRemaining)/totalTime) * 100;
+                pb.setProgress((int) percentageProgressBar);
+                Intent myIntent = new Intent(GameActivity.this, EndGame.class);
+                myIntent.putExtra("score", Integer.toString(score)); //Optional parameters
+                GameActivity.this.startActivity(myIntent);
+                finish();
+            }
+
+        };
+        timer.start();
+    }
+
+    void pauseGame(){
+        timer.cancel();
+        disableGrid();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Game Paused");
+        builder.setMessage("Would you like to continue game?");
+
+        // add the buttons
+        builder.setPositiveButton("Continue", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                resumeGame();
+            }
+        });
+        builder.setNegativeButton("Go Back", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent myIntent = new Intent(GameActivity.this, OptionsActivity.class);
+                GameActivity.this.startActivity(myIntent);
+                finish();
+            }
+        });
+
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        pauseGame();
     }
 
 
